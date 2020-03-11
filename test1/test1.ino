57691399ce2dc83e67c20635d924e51f1c9af900
pico0976313036 <62044219+pico0976313036@users.noreply.github.com>
2020-03-11 11:32:13 +0800

    Add files via upload

---
+++ test1/test1.ino
@@ -0,0 +1,24 @@
+const byte LEDs[] =
+{0,1,2,3,4,5,6,7};
+const byte total = sizeof(LEDs);
+byte index = 0;
+void setup(){
+// 將每個 Pin 設定成輸出
+for (byte i=0;i<total;i++)
+{
+pinMode(LEDs[i], OUTPUT);
+}
+}
+
+void loop() {
+for (index=0; index <= 7; index++){
+for (byte i=0;i<total;i++) {
+digitalWrite(LEDs[i], LOW);}
+digitalWrite(LEDs[index], HIGH);
+delay(1000);}
+for (index=7; index > 0; index--){
+for (byte i=0;i<total;i++) {
+digitalWrite(LEDs[i], LOW);}
+digitalWrite(LEDs[index], HIGH);
+delay(1000);}
+}
